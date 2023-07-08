64f1b5031a004b636185f7cdb4aee63c13df4156
wong561 <88598378+wong561@users.noreply.github.com>
2023-07-07 21:35:31 -0400

    push!

--- library_project/book_app/models.py
+++ library_project/book_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
+
+
 
 # Create your models here.
+class Author(models.Model):
+    
+    name=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("authors_detail", kwargs={"pk": self.id})
+    
